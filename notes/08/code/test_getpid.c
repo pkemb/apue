8d0bc18a5f7c0c7690c75245509548a710ac5f58
pk <pk@pk1206.cn>
2020-03-02 22:25:20 +0800

    add test_getpid.c
    Change-Id: I95305e8f67fae675443f5c43fcfcd0adf6cfb4d9

---
+++ notes/08/code/test_getpid.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main()
+{
+    printf("pid  = %d\n", getpid());
+    printf("ppid = %d\n", getppid());
+    printf("uid  = %d\n", getuid());
+    printf("euid = %d\n", geteuid());
+    printf("gid  = %d\n", getgid());
+    printf("egid = %d\n", getegid());
+
+    return 0;
+}
