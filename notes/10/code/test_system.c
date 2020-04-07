6ba40a2b8c80b81e9d9a148cef4fdb3c77b8bd88
pkemb <pk@pkemb.com>
2020-04-07 23:44:59 +0800

    完善test_system.c
    Change-Id: Ief5a7a54216665227c18a163be804ac90f16c52f

--- notes/10/code/test_system.c
+++ notes/10/code/test_system.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
 
 int main()
 {
     char cmd[256] = {0};
-    int ret;
+    int status;
     char *ch;
 
     while (printf("cmd = "), fgets(cmd, 256, stdin) == cmd)
@@ -14,9 +16,29 @@ int main()
         ch = strchr(cmd, '\n');
         if (ch)
             *ch = 0;
-        printf("cmd = %s\n", cmd);
-        ret = system(cmd);
-        printf("system ret = 0x%x\n", ret);
+        //printf("cmd = %s\n", cmd);
+        status = system(cmd);
+        printf("system ret = 0x%x\n", status);
+        if (status == -1)
+        {
+            printf("system error!!!\n");
+        }
+        else if (WIFEXITED(status))
+        {
+            printf("normal termination, exit code = %d\n", WEXITSTATUS(status));
+        }
+        else if (WIFSIGNALED(status))
+        {
+            printf("killed by signal, signal = %d\n", WTERMSIG(status));
+        }
+        else if (WIFSTOPPED(status))
+        {
+            printf("stopped by signal, signal = %d\n", WSTOPSIG(status));
+        }
+        else
+        {
+            printf("unknown status\n");
+        }
     }
     return 0;
 }
